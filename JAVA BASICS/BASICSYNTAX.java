4fa8c0440ead0a5cdcc8c830fdec9853b976515d
MPOYI TSHIBUYI <68503196+Frank1963-mpoyi@users.noreply.github.com>
2025-01-04 09:39:07 +0200

    setup

---
+++ JAVA BASICS/BASICSYNTAX.java
@@ -0,0 +1,17 @@
+
+
+public class BASICSYNTAX {
+  
+    // Program begins with a call to main() method
+    public static void main(String args[])
+    {
+      
+          // Prints "Hello World" 
+        System.out.println("Hello World");
+    }
+}
+
+
+// How to run the code , you must compile the code before you run it
+//javac BASICSYNTAX.java
+//java BASICSYNTAX
